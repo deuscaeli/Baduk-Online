e6822b3e6baf92d6f45138ea21dac430c4d0b733
Ziyang Yu <yuziyang7@outlook.com>
2021-01-15 10:29:09 +0800

    deployment path edit

--- config.sh
+++ config.sh
@@ -1,9 +1,9 @@
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 echo "module.exports = {
     // mongoUrl: 'mongodb://localhost:27017/baduk_online',
-    mongoUrl: 'mongodb+srv://baduk:baduk@cluster0.319wl.mongodb.net/baduk_online?retryWrites=true&w=majority',
+    mongoUrl: 'mongodb+srv://yuziy:yC9cETXDhq1yVWLq@cluster0.ikpyo.gcp.mongodb.net/messages?retryWrites=true&w=majority',
     JWT_SECRET: 'my secret'
 }" > $SHELL_FOLDER/Baduk-Online-Backend/env.js
 
-echo "const SERVER_URL = 'http://35.226.255.75:7777/'
+echo "const SERVER_URL = 'http://172.17.16.97:7777/'
 export {SERVER_URL}" > $SHELL_FOLDER/Baduk-Online-Frontend/src/config.js
